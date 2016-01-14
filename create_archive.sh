81bb9004565dacde610808892701bb8ced603941
Sean Krail <i@seankrail.com>
2016-01-13 19:57:13 -0500

    0.3: New version release

--- create_archive.sh
+++ create_archive.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#
+# Parameters:  $1  -  Name of archive
+#              $2  -  Message for commit
+#
 if [[ $# -ne 2 ]]; then
-  echo "Have $# argument(s), expected 2. It's the name of the archive."
+  echo "Have $# argument(s), expected 2."
   exit 1
 fi
 find . -name '*.DS_Store' -type f -delete
-echo "$1: $2"
-echo '"$1: $2"'
-echo ""$1: $2""
 git add *
 git commit -m "$1: $2"
 git push origin master
