059f2ab865f056d7eef4a36d63b72dce70602c84
Sean Krail <i@seankrail.com>
2016-01-25 17:08:27 -0500

    0.3.159: Utilizing GitHub's API in order to import more projects (GitHub repos)

--- root/server.js
+++ root/server.js
@@ -36,11 +36,11 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.locals.theme = process.env.THEME; //Make the THEME environment variable available to the app.
-app.locals.version = fs.readFileSync('./version.txt', 'utf8').replace(/\n$/, '');
+app.locals.version = fs.readFileSync(path.join(__dirname, 'version.version'), 'utf8').replace(/\n$/, '');
 
 
 // Read config values from a JSON file.
-var config = fs.readFileSync('./app_config.json', 'utf8');
+var config = fs.readFileSync(path.join(__dirname, 'app_config.json'), 'utf8');
 config = JSON.parse(config);
 config.VERSION = app.locals.version;
 
@@ -53,10 +53,9 @@ var sns = new AWS.SNS({region: config.AWS_REGION});
 // Convert base.scss file into style.css (Async)
 sass.render(
   {
-    file: './public/scss/base.scss',
+    file: path.join(__dirname, 'public/scss/base.scss'),
     outputStyle: 'compressed'
-  },
-  function(err, data) {
+  }, (err, data) => {
     if (err) throw err;
     else fs.writeFile(path.join(__dirname, 'public/css/style.min.css'), data.css, 'utf8', (err) => {
       if (err) throw err;
@@ -65,29 +64,25 @@ sass.render(
 );
 sass.render(
   {
-    file: './public/scss/base.scss',
+    file: path.join(__dirname, 'public/scss/base.scss'),
     outputStyle: 'nested'
-  },
-  function(err, data) {
+  }, (err, data) => {
     if (err) throw err;
-    else fs.writeFile(path.join(__dirname, 'public/css/style.css'), data.css, 'utf8', (err) => {
-      if (err) throw err;
-    });
+    else fs.writeFile(path.join(__dirname, 'public/css/style.css'), data.css, 'utf8',(err) => { if (err) throw err; });
   }
 );
 
 // Minify javascript files
-var jsDir = 'public/js'
-var files = fs.readdir(path.join(__dirname, 'public/js'), (err, files) => {
+const jsDir = 'public/js'
+fs.readdir(path.join(__dirname, 'public/js'), (err, files) => {
+  if (err) throw err;
   files.forEach(
-    function(element, index, array) {
+    (element, index, array) => {
       new compressor.minify({
         type: 'gcc',
-        fileIn: './' + jsDir + '/' + element,
-        fileOut: './' + jsDir + '/' + element.replace(/\.[^/.]+$/, "") + '.min.js',
-        callback: function(err, min) {
-          if (err) throw err;
-        }
+        fileIn: path.join(__dirname, jsDir, element),
+        fileOut: path.join(__dirname, jsDir, element.replace(/\.[^/.]+$/, "") + '.min.js'),
+        callback: (err, min) => { if (err) throw err; }
       });
     }
   );
@@ -99,7 +94,7 @@ var files = fs.readdir(path.join(__dirname, 'public/js'), (err, files) => {
 fs.readdir(path.join(__dirname, 'public/static/content/projects'), (err, files) => {
   if (err) throw err;
   files.forEach(
-    function (element, index, array) {
+    (element, index, array) => {
       fs.readFile(path.join(__dirname, 'public/static/content/projects', element), 'utf8', (err, data) => {
         if (err) throw err;
         routes.projects.projects.hard.push(JSON.parse(data));
@@ -116,7 +111,7 @@ var github = new GitHubAPI({
   protocol: 'https',
   host: 'api.github.com'
 });
-var token = fs.readFileSync('./token.txt', 'utf8');
+const token = fs.readFileSync(path.join(__dirname, 'token.token'), 'utf8');
 github.authenticate({
   type: 'oauth',
   token: token
@@ -128,13 +123,12 @@ github.repos.getFromUser(
     sort: 'created',
     direction: 'desc',
     per_page: 10
-  },
-  function(err, data) {
+  }, (err, data) => {
     if (err) throw err;
-    if(!Array.isArray(data)) console.log('Error. GitHub data is not an array: ', data);
+    if(!Array.isArray(data)) console.error('Error. GitHub data is not an array: ', data);
     else {
       data.forEach(
-        function(element, index, array) {
+        (element, index, array) => {
           var project = {
             id: element.name,
             header: {
@@ -157,7 +151,7 @@ github.repos.getFromUser(
           https.get('https://raw.githubusercontent.com/' + element.full_name + '/master/README.md', (res) => {
             var data = '';
             res.on('data', (d) => { data += d; });
-            res.on('end', function() {
+            res.on('end', () => {
               project.content[0].html = mdConverter(data);
               routes.projects.projects.soft.push(project);
             });
@@ -174,13 +168,11 @@ github.repos.getFromUser(
 
 
 
-// GET home page.
-app.get('/', routes.home);
-
 // GET each page in the /routes/ directory
 Object.keys(routes).forEach(
-  function(element, index, array) {
-    if (element === 'projects') {
+  (element, index, array) => {
+    if (element === 'home') app.get('/', '/home', routes.home);
+    else if (element === 'projects') {
       app.get('/projects', (req, res) => {
         res.render('projects', {
           page: 'projects',
@@ -194,7 +186,7 @@ Object.keys(routes).forEach(
 
 
 // POST signup form.
-app.post('/signup', function(req, res) {
+app.post('/signup', (req, res) => {
   var nameField = req.body.name,
       emailField = req.body.email,
       previewBool = req.body.previewAccess;
@@ -203,14 +195,14 @@ app.post('/signup', function(req, res) {
 });
 
 // POST project search
-app.post('/projects', function(req, res) {
+app.post('/projects', (req, res) => {
   var searchField = req.body.search;
   res.send(200);
   search(searchField);
 });
 
 // Add signup form data to database.
-var signup = function (nameSubmitted, emailSubmitted, previewPreference) {
+var signup = function(nameSubmitted, emailSubmitted, previewPreference) {
   var formData = {
     TableName: config.STARTUP_SIGNUP_TABLE,
     Item: {
@@ -219,15 +211,15 @@ var signup = function (nameSubmitted, emailSubmitted, previewPreference) {
       preview: {'S': previewPreference}
     }
   };
-  db.putItem(formData, function(err, data) {
+  db.putItem(formData, (err, data) => {
     if (err) throw err;
     else {
-      console.log('Form data added to database.');
+      //console.log('Form data added to database.');
       var snsMessage = 'New signup: %EMAIL%'; //Send SNS notification containing email from form.
       snsMessage = snsMessage.replace('%EMAIL%', formData.Item.email['S']);
-      sns.publish({ TopicArn: config.NEW_SIGNUP_TOPIC, Message: snsMessage }, function(err, data) {
+      sns.publish({ TopicArn: config.NEW_SIGNUP_TOPIC, Message: snsMessage }, (err, data) => {
         if (err) throw err;
-        else console.log('SNS message sent.');
+        //console.log('SNS message sent.');
       });  
     }
   });
@@ -243,6 +235,6 @@ var search = function(searchSubmitted) {
   };
 };
 
-http.createServer(app).listen(app.get('port'), function() {
+http.createServer(app).listen(app.get('port'), () => {
   console.log('Express server listening on port ' + app.get('port'));
 });
