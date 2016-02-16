c052a71a894a4da313d4efc9b1450d4586f13d02
Sean Krail <i@seankrail.com>
2016-02-15 23:18:30 -0500

    Changed scope of repo

--- root/routes/projects.js
+++
@@ -1,5 +0,0 @@
-module.exports = require('../public/static/content/header-footer/header-footer.js');
-
-module.exports.projects = [];
-
-module.exports.keywords = [];
--- seankrail/routes/index.js
+++
@@ -1,8 +0,0 @@
-/* GET home page */
-exports.home = require('./home.js');
-
-/* GET resume page */
-exports.resume = require('./resume.js');
-
-/* GET projects page */
-exports.projects = require('./projects.js');
